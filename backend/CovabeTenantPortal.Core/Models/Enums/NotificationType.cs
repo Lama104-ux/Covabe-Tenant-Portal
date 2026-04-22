82b9777263c09a792b56d46c25b57fdc2df6020c
Lama <lamamamoun90@gmail.com>
2026-04-22 15:20:15 +0200

    Add NotificationType enum with defined notification types

---
+++ backend/CovabeTenantPortal.Core/Models/Enums/NotificationType.cs
@@ -0,0 +1,10 @@
+namespace CovabeTenantPortal.Core.Models.Enums;
+
+public enum NotificationType
+{
+    NewMessage,
+    NewNews,
+    MaintenanceUpdate,
+    ContractExpiring,
+    InvitationAccepted
+}
