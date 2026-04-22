d863fac73e553f043b2f847eaa0d8954303878d2
Lama <lamamamoun90@gmail.com>
2026-04-22 15:21:48 +0200

    Add NotificationConfiguration class for Entity Framework setup

---
+++ backend/CovabeTenantPortal.Infrastructure/Data/Configurations/NotificationConfiguration.cs
@@ -0,0 +1,19 @@
+using CovabeTenantPortal.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CovabeTenantPortal.Infrastructure.Data.Configurations;
+
+public class NotificationConfiguration : IEntityTypeConfiguration<Notification>
+{
+    public void Configure(EntityTypeBuilder<Notification> builder)
+    {
+        builder.Property(n => n.Title).HasMaxLength(200);
+        builder.Property(n => n.Body).HasMaxLength(1000);
+
+        builder.HasOne(n => n.User)
+            .WithMany(u => u.Notifications)
+            .HasForeignKey(n => n.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
